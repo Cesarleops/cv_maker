dac80e470693a354553879e0fc03a174cfa42428
Cesar Leyton <cesarleyton549@gmail.com>
2024-01-02 15:58:27 -0500

    Created header of the landing page and bento design for the curriculum information.

---
+++ app/components/header.tsx
@@ -0,0 +1,18 @@
+
+export const Header = () => {
+
+    return(
+        <header className="w-screen p-5 flex items-center">
+            <h1 className="mr-auto">Here you will see an awesome name</h1>
+            <ul className="flex gap-5">
+                <li>Sign In</li>
+                <li>
+                    <button>EN | ES
+                    </button>
+                    
+                </li>
+                
+            </ul>
+        </header>
+    )
+}
